f1dfd0113f13b4d0fc16224f7c656da3753c86f6
Ершов Даниил <daer0508@mail.ru>
2026-02-11 15:02:13 +0400

    Java / Задание 1

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,15 +1,33 @@
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+import java.util.Random;
 
 public class BankAccount {
+
     private String ownerName;
     private int balance;
     private LocalDateTime openingDate;
     private boolean isBlocked;
+    private String accountNumber;
+
+
     public BankAccount(String ownerName) {
         this.ownerName = ownerName;
         this.balance = 0;
         this.openingDate = LocalDateTime.now();
         this.isBlocked = false;
+        this.accountNumber = generateAccountNumber();
+    }
+
+
+    private String generateAccountNumber() {
+        Random random = new Random();
+        StringBuilder number = new StringBuilder();
+        for (int i = 0; i < 8; i++) {
+            number.append(random.nextInt(10));
+        }
+        return number.toString();
     }
 
 
@@ -28,6 +46,11 @@ public class BankAccount {
     public boolean isBlocked() {
         return isBlocked;
     }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
 
     public void setBlocked(boolean blocked) {
         isBlocked = blocked;
@@ -62,7 +85,58 @@ public class BankAccount {
         return false;
     }
 
+
     @Override
     public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+        String status = isBlocked ? "ЗАБЛОКИРОВАН" : "АКТИВЕН";
+
+        return "===================================\n" +
+                "         БАНКОВСКИЙ СЧЕТ          \n" +
+                "===================================\n" +
+                " Номер счета: " + accountNumber + "\n" +
+                " Владелец:    " + ownerName + "\n" +
+                " Баланс:      " + balance + " руб.\n" +
+                " Дата откр.:  " + openingDate.format(formatter) + "\n" +
+                " Статус:      " + status + "\n" +
+                "===================================";
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+            return true;
+        }
+
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+
+        BankAccount other = (BankAccount) obj;
+
+
+        return Objects.equals(accountNumber, other.accountNumber);
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountNumber);
+    }
+
+
+    public boolean equalsDetailed(BankAccount other) {
+        if (this == other) return true;
+        if (other == null) return false;
+
+        return Objects.equals(accountNumber, other.accountNumber) &&
+                Objects.equals(ownerName, other.ownerName) &&
+                balance == other.balance &&
+                Objects.equals(openingDate, other.openingDate) &&
+                isBlocked == other.isBlocked;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,86 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("=== ТЕСТИРОВАНИЕ БАНКОВСКИХ СЧЕТОВ ===\n");
+
+
         BankAccount account1 = new BankAccount("Иван Петров");
         BankAccount account2 = new BankAccount("Мария Иванова");
+        BankAccount account3 = new BankAccount("Иван Петров");
 
 
-
-
-
+        System.out.println("ЗАДАНИЕ 1: МЕТОД toString()");
+        System.out.println("-----------------------------------");
+        System.out.println("Счет №1:");
         System.out.println(account1);
+        System.out.println();
+        System.out.println("Счет №2:");
         System.out.println(account2);
+        System.out.println();
+
+
+        System.out.println("ЗАДАНИЕ 2: НОМЕРА СЧЕТОВ");
+        System.out.println("-----------------------------------");
+        System.out.println("Номер счета 1: " + account1.getAccountNumber());
+        System.out.println("Номер счета 2: " + account2.getAccountNumber());
+        System.out.println("Номер счета 3: " + account3.getAccountNumber());
+        System.out.println();
+
+
+        account1.deposit(5000);
+        account2.deposit(3000);
+        account3.deposit(10000);
+
+        account1.transfer(account2, 1500);
+        account2.setBlocked(true);
+
+
+        System.out.println("ЗАДАНИЕ 3: МЕТОДЫ equals() И hashCode()");
+        System.out.println("-----------------------------------");
+
+
+        System.out.println("account1.equals(account1): " + account1.equals(account1));
+
+
+        System.out.println("account1.equals(account2): " + account1.equals(account2));
+
+
+        System.out.println("account1.equals(account3): " + account1.equals(account3));
+
+
+        System.out.println("\nХеш-коды счетов:");
+        System.out.println("account1 hash: " + account1.hashCode());
+        System.out.println("account2 hash: " + account2.hashCode());
+        System.out.println("account3 hash: " + account3.hashCode());
+
+
+        System.out.println("\naccount1.equals(null): " + account1.equals(null));
+
+
+        System.out.println("\nКонтракт equals/hashCode:");
+        System.out.println("account1.equals(account1): " + account1.equals(account1));
+        System.out.println("hashCode одинаковый: " +
+                (account1.hashCode() == account1.hashCode()));
+
+
+        System.out.println("\nИспользование в коллекциях:");
+        java.util.HashSet<BankAccount> accounts = new java.util.HashSet<>();
+        accounts.add(account1);
+        accounts.add(account2);
+        accounts.add(account3);
+        accounts.add(account1);
+
+        System.out.println("Размер HashSet: " + accounts.size() + " (должен быть 3)");
+        System.out.println("Счета в HashSet:");
+        for (BankAccount acc : accounts) {
+            System.out.println("  - " + acc.getAccountNumber() + " | " + acc.getOwnerName());
+        }
+
+
+        System.out.println("\n=== ФИНАЛЬНОЕ СОСТОЯНИЕ СЧЕТОВ ===");
+        System.out.println(account1);
+        System.out.println();
+        System.out.println(account2);
+        System.out.println();
+        System.out.println(account3);
     }
 }
